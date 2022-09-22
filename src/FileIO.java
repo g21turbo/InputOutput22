b1245a6196ca39f85159f2fe4e0ebe906803a475
User <dg21stew@gmail.com>
2022-09-21 23:40:40 -0700

    added array

--- src/FileIO.java
+++ src/FileIO.java
@@ -6,9 +6,17 @@ public class FileIO {
 
     public static void main(String[] args) {
 
+        String[] names = {"Manny", "Moe", "Jack"};
+
         try {
             BufferedWriter writer = new BufferedWriter(new FileWriter("output.txt"));
-            writer.write("Writing to a file.");
+            writer.write("Writing to a file.\n");
+            writer.write("Writing some more.");
+
+            for (String name : names) {
+                writer.write("\n" + name);
+            }
+
             writer.close();
         } catch (IOException e) {
             e.printStackTrace();
