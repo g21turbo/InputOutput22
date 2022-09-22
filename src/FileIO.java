347cca75f7bf97ab013523cf68f117dd81bd5349
User <dg21stew@gmail.com>
2022-09-21 23:36:27 -0700

    Initial commit

---
+++ src/FileIO.java
@@ -0,0 +1,20 @@
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class FileIO {
+
+    public static void main(String[] args) {
+
+        try {
+            BufferedWriter writer = new BufferedWriter(new FileWriter("output.txt"));
+            writer.write("Writing to a file.");
+            writer.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+    }
+
+}
